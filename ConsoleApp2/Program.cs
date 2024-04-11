447d385dfe5c1c22655f4617b8021abf5563524c
victoref <victoref3@gmail.com>
2024-04-11 11:27:40 +0200

    Loop works

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -15,13 +15,31 @@ while (boomer != boomer_do) {
 
     Console.WriteLine("What is the first number you would like to use in the Calculation");
 
+    Console.Write("number:\t");
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+    first_number = Convert.ToInt32(Console.ReadLine());
+      
+
 
 
 
     Console.WriteLine("what is the second number you would like to use");
 
 
+
+
+    Console.WriteLine("wanna do more? Yes or no");
+    string awnser = Console.ReadLine();
+    if(awnser == "yes" || awnser == "y")
+    {
+
+    }else
+    {
+        boomer = boomer_do;
+    }
+#pragma warning restore CS8600
 }
+
 
 
 
