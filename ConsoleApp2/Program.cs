9908f6783bf240175ea5da2225d80401f727045b
victoref <victoref3@gmail.com>
2024-04-11 11:08:16 +0200

    While boomer

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -5,13 +5,23 @@ string what_do;
 
 
 
+string boomer = "per";
+
+
+
+string boomer_do = "Work on Antwar";
+
+while (boomer != boomer_do) {
+
+    Console.WriteLine("What is the first number you would like to use in the Calculation");
 
 
 
 
+    Console.WriteLine("what is the second number you would like to use");
 
 
-
+}
 
 
 
